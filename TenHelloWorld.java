28e654c09e775fe30e18d1d6921e8b72ee9478c6
Zhouchengyu <marlonzhou_i@didiglobal.com>
2024-12-22 15:20:49 +0800

    init

---
+++ TenHelloWorld.java
@@ -0,0 +1,6 @@
+public class TenHelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      for (int x = 1;x<=10;x++) System.out.println("Hello, World");
+   }
+}
